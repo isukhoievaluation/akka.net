f2c382d0e6d7ff01da1925b3e20691dd4fcb7574
Ivan Sukhoi <ivan.sukhoi@hiqo-solutions.com>
2022-10-31 19:37:54 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# akka.net
+
+**WinTail:** *NIX systems have the tail command built-in to monitor changes to a file (such as tailing log files), whereas Windows does not. I recreated tail for Windows.
+
+**ChartApp:** I built my own version of Resource Monitor using Windows Forms, data visualization tools built into .NET, and [Performance Counters](https://msdn.microsoft.com/en-us/library/system.diagnostics.performancecounter.aspx "PerformanceCounter Class - C#").
+
+**GithubActors:** I built a sophisticated GitHub scraper that can simultaneously retrieve data from multiple GitHub repos at once.
+
+
---
+++ WinTail/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+﻿using Akka.Actor;
+
+namespace WinTail
+{
+    #region Program
+    class Program
+    {
+        public static ActorSystem MyActorSystem;
+
+        static void Main(string[] args)
+        {
+            // initialize MyActorSystem
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
+
+            //Props fakeActorProps = Props.Create(typeof(FakeActor));
+            //IActorRef fackeActor = MyActorSystem.ActorOf(fakeActorProps, "fackeActor");
+
+            Props consoleWriterProps = Props.Create<ConsoleWriterActor>();
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterProps");
+            Props tailCoordinatorProps = Props.Create(() => new TailCoordinatorActor());
+            IActorRef tailCoordinatorActor = MyActorSystem.ActorOf(tailCoordinatorProps, "tailCoordinatorActor");
+            Props fileValidatorActorProps = Props.Create(() => new FileValidatorActor(consoleWriterActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(fileValidatorActorProps, "validationActor");
+            Props consoleReaderProps = Props.Create<ConsoleReaderActor>();
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+
+            // tell console reader to begin
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MyActorSystem.WhenTerminated.Wait();
+        }
+    }
+
+    public class FakeActor { }
+    #endregion
+}
